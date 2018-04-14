10db32a911c72c82e821ff609fb03515d59ba5e1
Joël ZHU <>
2018-04-14 14:47:03 +0200

    Added sitemap

--- Budget_Tracker/Budget_Tracker/MasterPage.Master.designer.cs
+++ Budget_Tracker/Budget_Tracker/MasterPage.Master.designer.cs
@@ -31,6 +31,33 @@ namespace Budget_Tracker {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// GeneralSiteMap control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SiteMapDataSource GeneralSiteMap;
+        
+        /// <summary>
+        /// SiteMap1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SiteMapPath SiteMap1;
+        
+        /// <summary>
+        /// SiteMapList control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Menu SiteMapList;
+        
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
